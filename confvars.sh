8f461e3892caba16d2b2ca17a070ceff93973f1c
Matt A. Tobin <email@mattatobin.com>
2018-04-22 20:17:05 -0400

    Change the name of the application

--- confvars.sh
+++ confvars.sh
@@ -3,10 +3,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-MOZ_APP_BASENAME=SeaMonkey
-MOZ_APP_VENDOR=Mozilla
-MOZ_APP_NAME=seamonkey
-MOZ_APP_DISPLAYNAME=SeaMonkey
+MOZ_APP_BASENAME=Borealis
+MOZ_APP_VENDOR=BinOC
+MOZ_APP_NAME=borealis
+MOZ_APP_DISPLAYNAME=Borealis
 MOZ_DISABLE_EXPORT_JS=1
 MOZ_SUITE=1
 BINOC_BOREALIS=1
@@ -17,9 +17,9 @@ MOZ_UPDATER=1
 # This should usually be the same as the value MAR_CHANNEL_ID.
 # If more than one ID is needed, then you should use a comma separated list
 # of values.
-ACCEPTED_MAR_CHANNEL_IDS=seamonkey-comm-release
+ACCEPTED_MAR_CHANNEL_IDS=release
 # The MAR_CHANNEL_ID must not contain the following 3 characters: ",\t "
-MAR_CHANNEL_ID=seamonkey-comm-release
+MAR_CHANNEL_ID=release
 MOZ_SERVICES_FXACCOUNTS=
 MOZ_STATIC_BUILD_UNSUPPORTED=1
 MOZ_WEBGL_CONFORMANT=1
