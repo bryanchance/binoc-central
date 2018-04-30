620007a9618f517474b7e2a33b97b5eeb0eeedec
Matt A. Tobin <email@mattatobin.com>
2018-04-30 18:47:17 -0400

    Add Gecko Compatibility checkbox to HTTP Preferences

--- base/content/pref/pref-http.js
+++ base/content/pref/pref-http.js
@@ -28,31 +28,3 @@ function CheckPipeliningProxy()
   EnableElementById("enablePipeliningProxy", enabled, false);
 }
 
-// Lightning adds a UA token if calendar.useragent.extra is not empty.
-// Checkbox is visible if Lightning is installed and enabled.
-// Checkbox is checked if the pref contains any non-whitespace character.
-function CheckLightningUA()
-{
-  var prefLightningUA = document.getElementById("calendar.useragent.extra");
-  var boxLightningUA  = document.getElementById("uaLightningShow");
-
-  if (prefLightningUA.defaultValue)
-  {
-    boxLightningUA.hidden = false;
-    boxLightningUA.checked = prefLightningUA.value.trim() != "";
-    boxLightningUA.disabled = prefLightningUA.locked;
-    gLightningUAold = prefLightningUA.hasUserValue ? prefLightningUA.value : null;
-  }
-}
-
-// If the checkbox is checked, use previous user-set or default value.
-// If the checkbox is not checked, set the pref to an empty string.
-function OnLightningChanged(aChecked)
-{
-  var prefLightningUA = document.getElementById("calendar.useragent.extra");
-
-  if (aChecked && !gLightningUAold)
-    prefLightningUA.reset();
-  else
-    prefLightningUA.value = (aChecked && gLightningUAold) || "";
-}
